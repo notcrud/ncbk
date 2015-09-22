aa233548ae7453d809b5390a87a11be8e85212ed
Rudi MK <Rudi MK>
2015-09-22 14:16:14 +0530

    Updated ncbk to use plan instead of python-crontab.

--- ncbk.py
+++ ncbk.py
@@ -1,4 +1,5 @@
-from crontab import CronTab
+#from crontab import CronTab
+from plan import Plan
 
 '''
 This script uses python-crontab to run a cron job every day at noon,
@@ -6,8 +7,16 @@ to save a dump of all the keys in the Redis datastore that's used
 by notCRUD.
 '''
 
+'''
 tab = CronTab(user='rudimk')
 command = '/usr/bin/redis-cli bgsave'
 cron_job = tab.new(cmd, comment='This command dumps Redis keys and saves them.')
 cron_job.minute().on(0)
 cron_job.hour().on(12)
+'''
+
+cron = Plan()
+cron.command('/usr/bin/redis-cli bgsave', every='1.day', at='12:00')
+
+if __name__ == '__main__':
+	cron.run('write')
